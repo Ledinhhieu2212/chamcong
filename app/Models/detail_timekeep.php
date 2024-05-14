94e1a2c1e71bf73d86fe7194112e1d1901a2a425
ledinhhieu2212 <dinhhieu203765@gmail.com>
2024-05-14 10:45:06 +0700

    fix 2

--- app/Models/detail_timekeep.php
+++ app/Models/detail_timekeep.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class detail_timekeep extends Model
+class Detail_timekeep extends Model
 {
     use HasFactory;
     protected $fillable = [
--- app/Models/timekeep.php
+++ app/Models/timekeep.php
@@ -6,7 +6,7 @@ use App\Traits\UUID;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class timekeep extends Model
+class Timekeep extends Model
 {
     use HasFactory;
     protected $fillable = [
